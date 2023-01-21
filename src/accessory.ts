2cf74967d7c81e60c8806c8132c09338ee1cda2a
Al-Mclean <al_mclean@hotmail.com>
2023-01-21 14:58:07 +1030

    Update

--- src/accessory.ts
+++ src/accessory.ts
@@ -9,41 +9,16 @@ import {
   HAP,
   Logging,
   Service
-} from "homebridge";
+} from 'homebridge';
 
-/*
- * IMPORTANT NOTICE
- *
- * One thing you need to take care of is, that you never ever ever import anything directly from the "homebridge" module (or the "hap-nodejs" module).
- * The above import block may seem like, that we do exactly that, but actually those imports are only used for types and interfaces
- * and will disappear once the code is compiled to Javascript.
- * In fact you can check that by running `npm run build` and opening the compiled Javascript file in the `dist` folder.
- * You will notice that the file does not contain a `... = require("homebridge");` statement anywhere in the code.
- *
- * The contents of the above import statement MUST ONLY be used for type annotation or accessing things like CONST ENUMS,
- * which is a special case as they get replaced by the actual value and do not remain as a reference in the compiled code.
- * Meaning normal enums are bad, const enums can be used.
- *
- * You MUST NOT import anything else which remains as a reference in the code, as this will result in
- * a `... = require("homebridge");` to be compiled into the final Javascript code.
- * This typically leads to unexpected behavior at runtime, as in many cases it won't be able to find the module
- * or will import another instance of homebridge causing collisions.
- *
- * To mitigate this the {@link API | Homebridge API} exposes the whole suite of HAP-NodeJS inside the `hap` property
- * of the api object, which can be acquired for example in the initializer function. This reference can be stored
- * like this for example and used to access all exported variables and classes from HAP-NodeJS.
- */
 let hap: HAP;
 
-/*
- * Initializer function called when the plugin is loaded.
- */
 export = (api: API) => {
   hap = api.hap;
-  api.registerAccessory("ExampleSwitch", ExampleSwitch);
+  api.registerAccessory("Meter", Meter);
 };
 
-class ExampleSwitch implements AccessoryPlugin {
+class Meter implements AccessoryPlugin {                                                // Define Class
 
   private readonly log: Logging;
   private readonly name: string;
@@ -52,11 +27,12 @@ class ExampleSwitch implements AccessoryPlugin {
   private readonly switchService: Service;
   private readonly informationService: Service;
 
-  constructor(log: Logging, config: AccessoryConfig, api: API) {
+  constructor(log: Logging, config: AccessoryConfig, api: API) {                      // Class Constructor
     this.log = log;
     this.name = config.name;
 
     this.switchService = new hap.Service.Switch(this.name);
+    
     this.switchService.getCharacteristic(hap.Characteristic.On)
       .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
         log.info("Current state of the switch was returned: " + (this.switchOn? "ON": "OFF"));
@@ -73,12 +49,16 @@ class ExampleSwitch implements AccessoryPlugin {
       .setCharacteristic(hap.Characteristic.Model, "Custom Model");
 
     log.info("Switch finished initializing!");
-  }
+  }                                                                                      // End Class Constructor
+
+  // Now define Class methods
 
   /*
    * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
    * Typical this only ever happens at the pairing process.
    */
+
+
   identify(): void {
     this.log("Identify!");
   }
